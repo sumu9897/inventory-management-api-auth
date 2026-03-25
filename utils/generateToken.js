c823b5b161edeb612df39f98694235c8260177b7
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2026-03-25 16:55:35 +0600

    feat: implement JWT authentication token generator

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -16,7 +16,7 @@ const generateToken = (res, user) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000
+    maxAge: 1 * 24 * 60 * 60 * 1000
   });
 
   return token;
