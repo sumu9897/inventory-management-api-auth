6921a6d4f9ed05dc79b0711a2414f8b82eed6380
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2026-03-25 16:54:23 +0600

    feat: implement JWT authentication token generator

---
+++ utils/generateToken.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (res, user) => {
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN || "1d"
+    }
+  );
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  });
+
+  return token;
+};
+
+module.exports = generateToken;
